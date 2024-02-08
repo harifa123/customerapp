a930cbdbf255b1366d127758eedcdd6addb5ccb0
harifa123 <harifarifu@gmail.com>
2024-02-09 00:05:03 +0530

    updated

--- controller/customerRouter.js
+++ controller/customerRouter.js
@@ -18,4 +18,11 @@ router.get("/viewall",async(req,res)=>{
     res.json(data)
 })
 
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await costomerModel.find(input)
+
+    res.json(data)
+})
+
 module.exports=router
